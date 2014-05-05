94a59c7246457a7eed5901c2a6e666c99d295096
Tommy <lokoi_53@hotmail.fr>
2014-05-05 11:51:47 +0200

    first commit

---
+++ Yahtzee-IIA/Yahtzee-IIA/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Yahtzee_IIA.Resources;
+
+namespace Yahtzee_IIA
+{
+    /// <summary>
+    /// Permet d'accéder aux ressources de chaîne.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
