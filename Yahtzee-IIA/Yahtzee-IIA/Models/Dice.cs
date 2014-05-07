5538a918c81d3f934fa4a7c3edbc4a580bb48bc5
Tommy <lokoi_53@hotmail.fr>
2014-05-07 15:18:14 +0200

    FIX  - Commit à la mano

--- Yahtzee-IIA/Yahtzee-IIA/Models/Dice.cs
+++ Yahtzee-IIA/Yahtzee-IIA/Models/Dice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq.Mapping;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,30 +8,34 @@ using WP.Core;
 
 namespace Yahtzee_IIA.Models
 {
+    [Table]
     class Dice : ObservableObject
     {
         #region Fields
 
-        private int _number; 
-        private Boolean _keep; 
+        private int _number;
+        private Boolean _keep;
         private String _image;
 
         #endregion
 
         #region Properties
 
+        [Column(DbType = "Integer")]
         public int Number
         {
             get { return _number; }
             set { Assign(ref _number, value); }
         }
 
+        [Column(DbType = "Boolean", CanBeNull = false)]
         public Boolean Keep
         {
             get { return _keep; }
             set { Assign(ref _keep, value); }
         }
 
+        [Column(DbType = "NVarChar(1024)", CanBeNull = false)]
         public String Image
         {
             get { return _image; }
--- Yahtzee-IIA/Yahtzee-IIA/Models/Game.cs
+++ Yahtzee-IIA/Yahtzee-IIA/Models/Game.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,35 +9,36 @@ using WP.Core;
 
 namespace Yahtzee_IIA.Models
 {
-    class Game : ObservableObject
+    [Table]
+    public class Game : ObservableObject
     {
         #region Fields
 
-        /// <summary>
-        ///     Tableau des joueurs
-        /// </summary>
-        private Player[] _aPlayers; 
+        private long _id;
+        private EntitySet<Player> _aPlayers;
 
         /// <summary>
         ///     Nombre de lancers restants
         /// </summary>
-	    private int _nbRoll; 
+        private int _nbRoll;
 
         /// <summary>
         ///     Tableau des 5 dés
         /// </summary>
-	    private Dice[] _dices;
+        private Dice[] _dices;
 
         #endregion
 
         #region Properties
 
-        public Player[] aPlayers 
+        [Association(Storage = "Players", OtherKey = "IdPlayer")]
+        public EntitySet<Player> Players
         {
             get { return _aPlayers; }
-            set { Assign(ref _aPlayers, value); }
-         }
+            set { _aPlayers.Assign(value); }
+        }
 
+        [Column(DbType = "Integer", CanBeNull = false)]
         public int NbRoll
         {
             get { return _nbRoll; }
@@ -58,7 +61,8 @@ namespace Yahtzee_IIA.Models
         /// <param name="nbPlayers">Nombre de joueurs de la partie</param>
         public Game(int nbPlayers)
         {
-            _aPlayers = new Player[nbPlayers];
+            //_aPlayers = new Player[nbPlayers];
+            _aPlayers = new EntitySet<Player>(AttachPlayer, DetachPlayer);
             _nbRoll = 3;
 
             for (int i = 0; i < 5; i++)
@@ -72,10 +76,22 @@ namespace Yahtzee_IIA.Models
 
         #region Methods
 
+        private void AttachPlayer(Player player)
+        {
+            player.Game = this;
+            OnPropertyChanged("Players");
+        }
+
+        private void DetachPlayer(Player player)
+        {
+            player.Game = null;
+            OnPropertyChanged("Players");
+        }
+
         /// <summary>
         ///     Appelle la fonction random pour les dés dont la propriété keep=false
         /// </summary>
-        public void roll() 
+        public void roll()
         {
 
         }
@@ -84,7 +100,7 @@ namespace Yahtzee_IIA.Models
         ///     Génère un nombre aléatoire entre 1 et 5 pour le dé passé en paramètre
         /// </summary>
         /// <param name="dice">Dé sur lequel effectuer le tirage</param>
-	    public void random (Dice dice) 
+        public void random(Dice dice)
         {
             //TODO: mettre à jour la propriété « number » du dé passé en paramètre avec le nombre aléatoire
         }
